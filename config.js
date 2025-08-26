58bd895f33d32e54135b5559984a56b392685c96
Seth Iradukunda <sethiradukunda113@gmail.com>
2025-08-26 05:30:00 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-
-// Replace with your Supabase project URL and anon public key
-const SUPABASE_URL = "https://gmfgchuizwemqcafdtcd.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtZmdjaHVpendlbXFjYWZkdGNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxOTg5MDIsImV4cCI6MjA3MTc3NDkwMn0.uchcIzuwXntPCxLPlOQQ7YgEyTd5xI7lAHt33F365C4";
-
-// Initialize Supabase client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
